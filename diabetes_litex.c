9077a8dbe224f940e00ee03590524cba4138d8b2
Bruno Sobreira Franca <brunosfranca15@gmail.com>
2025-06-21 16:59:37 -0300

    Sync repository

--- diabetes_litex.c
+++ diabetes_litex.c
@@ -1,14 +1,24 @@
+#include <string.h>
 #include <stdio.h>
 #include <stddef.h>
 #include <generated/csr.h>
 #include <irq.h>
-#include <uart.h>
+#include <libbase/uart.h>
+#include <libbase/console.h>
+
 #include "inference_accel.h"
 
 uint32_t start_ticks;
 uint32_t elapsed_ticks;
 
-void start_stopwatch() {
+void start_stopwatch(void);
+void stop_stopwatch(void);
+void print_elapsed_time(uint32_t ticks, const char* benchmark_name);
+double predict(double x);
+int predict_int(double x);
+int benchmark(void);
+
+void start_stopwatch(void) {
     // Disable timer
     timer0_en_write(0);
     
@@ -64,17 +74,17 @@ int predict_int(double x) {
     return (((int)(x * 100)) * 93823 + 1529188) / 100;
 }
 
-int main() {
-    printf("LiteX Benchmark with Hardware Accelerator Starting...\n");
+int benchmark(void) {
+    puts("LiteX Benchmark with Hardware Accelerator Starting...\n");
     
     double input = 0.03; // valor da feature
     volatile double p1 = 0;
     volatile int p2 = 0;
-    volatile int p3 = 0; // Hardware accelerator results
     int i;
     
     // Initialize hardware accelerator
 #ifdef CSR_INFERENCE_ACCEL_BASE
+    volatile int p3 = 0; // Hardware accelerator results
     printf("Initializing inference accelerator...\n");
     inference_accel_init();
     inference_accel_set_params(938.237861251353, 152.91886182616113);
@@ -157,4 +167,148 @@ int main() {
     
     printf("\nBenchmark completed!\n");
     return 0;
+}
+
+
+/*-----------------------------------------------------------------------*/
+/* Uart                                                                  */
+/*-----------------------------------------------------------------------*/
+
+static char *readstr(void)
+{
+	char c[2];
+	static char s[64];
+	static int ptr = 0;
+
+	if(readchar_nonblock()) {
+		c[0] = getchar();
+		c[1] = 0;
+		switch(c[0]) {
+			case 0x7f:
+			case 0x08:
+				if(ptr > 0) {
+					ptr--;
+					fputs("\x08 \x08", stdout);
+				}
+				break;
+			case 0x07:
+				break;
+			case '\r':
+			case '\n':
+				s[ptr] = 0x00;
+				fputs("\n", stdout);
+				ptr = 0;
+				return s;
+			default:
+				if(ptr >= (sizeof(s) - 1))
+					break;
+				fputs(c, stdout);
+				s[ptr] = c[0];
+				ptr++;
+				break;
+		}
+	}
+
+	return NULL;
+}
+
+static char *get_token(char **str)
+{
+	char *c, *d;
+
+	c = (char *)strchr(*str, ' ');
+	if(c == NULL) {
+		d = *str;
+		*str = *str+strlen(*str);
+		return d;
+	}
+	*c = 0;
+	d = *str;
+	*str = c+1;
+	return d;
+}
+
+static void prompt(void)
+{
+	printf("\e[92;1mBruno-luiz-app\e[0m> ");
+}
+
+/*-----------------------------------------------------------------------*/
+/* Help                                                                  */
+/*-----------------------------------------------------------------------*/
+
+static void help(void)
+{
+	puts("\nLiteX minimal demo app built "__DATE__" "__TIME__"\n");
+	puts("Available commands:");
+	puts("help                              - Show this command");
+	puts("reboot                            - Reboot CPU");
+	puts("hello                             - Hello world");
+    puts("benchmark                         - benchmark");
+}
+
+
+/*-----------------------------------------------------------------------*/
+/* Commands                                                              */
+/*-----------------------------------------------------------------------*/
+
+static void reboot_cmd(void)
+{
+	ctrl_reset_write(1);
+}
+
+
+static void helloc_cmd(void)
+{
+	printf("Hello C demo...\n");
+}
+
+
+/*-----------------------------------------------------------------------*/
+/* Console service / Main                                                */
+/*-----------------------------------------------------------------------*/
+
+static void console_service(void)
+{
+	char *str;
+	char *token;
+
+	str = readstr();
+	if(str == NULL) return;
+	token = get_token(&str);
+	if(strcmp(token, "help") == 0)
+    {
+		help();
+    } 
+    else if(strcmp(token, "reboot") == 0)
+    {
+		reboot_cmd();
+    } 
+    else if(strcmp(token, "hello") == 0) 
+    {
+		helloc_cmd();
+    } 
+    else if(strcmp(token, "benchmark") == 0)
+    {
+        benchmark();
+    }
+	prompt();
+}
+
+int main(void)
+{
+#ifdef CONFIG_CPU_HAS_INTERRUPT
+	irq_setmask(0);
+	irq_setie(1);
+#endif
+	uart_init();
+
+	help();
+	prompt();
+
+	while(1) {
+		console_service();
+	}
+
+	return 0;
 }
---
+++ diabetes_litex.d
@@ -0,0 +1,65 @@
+diabetes_litex.o: diabetes_litex.c \
+ /home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/string.h \
+ /home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/_ansi.h \
+ /home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/newlib.h \
+ /home/hilb/git/unicamp/mo801/mo801-projeto3/build/sipeed_tang_nano_9k/software/include/../libc/picolibc.h \
+ /home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/config.h \
+ /home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/ieeefp.h \
+ /home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/features.h \
+ /usr/lib/gcc/riscv64-unknown-elf/13.2.0/include/float.h \
+ /home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/cdefs.h \
+ /home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_default_types.h \
+ /usr/lib/gcc/riscv64-unknown-elf/13.2.0/include/stddef.h \
+ /home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_locale.h \
+ /home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/strings.h \
+ /home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_types.h \
+ /home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_types.h \
+ /home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/string.h \
+ /home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/tinystdio/stdio.h \
+ /home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/inttypes.h \
+ /home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_intsup.h \
+ /home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdint.h \
+ /home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_stdint.h \
+ /usr/lib/gcc/riscv64-unknown-elf/13.2.0/include/stdarg.h \
+ /home/hilb/git/unicamp/mo801/mo801-projeto3/build/sipeed_tang_nano_9k/software/include/generated/csr.h \
+ /home/hilb/git/unicamp/mo801/mo801-projeto3/build/sipeed_tang_nano_9k/software/include/generated/soc.h \
+ /home/hilb/git/unicamp/mo801/litex/litex/litex/soc/cores/cpu/vexriscv/system.h \
+ /home/hilb/git/unicamp/mo801/litex/litex/litex/soc/cores/cpu/vexriscv/csr-defs.h \
+ /home/hilb/git/unicamp/mo801/litex/litex/litex/soc/software/include/hw/common.h \
+ /home/hilb/git/unicamp/mo801/litex/litex/litex/soc/software/include/irq.h \
+ /home/hilb/git/unicamp/mo801/litex/litex/litex/soc/cores/cpu/vexriscv/irq.h \
+ /home/hilb/git/unicamp/mo801/litex/litex/litex/soc/software/libbase/uart.h \
+ /home/hilb/git/unicamp/mo801/litex/litex/litex/soc/software/libbase/console.h \
+ inference_accel.h
+/home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/string.h:
+/home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/_ansi.h:
+/home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/newlib.h:
+/home/hilb/git/unicamp/mo801/mo801-projeto3/build/sipeed_tang_nano_9k/software/include/../libc/picolibc.h:
+/home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/config.h:
+/home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/ieeefp.h:
+/home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/features.h:
+/usr/lib/gcc/riscv64-unknown-elf/13.2.0/include/float.h:
+/home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/cdefs.h:
+/home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_default_types.h:
+/usr/lib/gcc/riscv64-unknown-elf/13.2.0/include/stddef.h:
+/home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_locale.h:
+/home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/strings.h:
+/home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_types.h:
+/home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_types.h:
+/home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/string.h:
+/home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/tinystdio/stdio.h:
+/home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/inttypes.h:
+/home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_intsup.h:
+/home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdint.h:
+/home/hilb/git/unicamp/mo801/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_stdint.h:
+/usr/lib/gcc/riscv64-unknown-elf/13.2.0/include/stdarg.h:
+/home/hilb/git/unicamp/mo801/mo801-projeto3/build/sipeed_tang_nano_9k/software/include/generated/csr.h:
+/home/hilb/git/unicamp/mo801/mo801-projeto3/build/sipeed_tang_nano_9k/software/include/generated/soc.h:
+/home/hilb/git/unicamp/mo801/litex/litex/litex/soc/cores/cpu/vexriscv/system.h:
+/home/hilb/git/unicamp/mo801/litex/litex/litex/soc/cores/cpu/vexriscv/csr-defs.h:
+/home/hilb/git/unicamp/mo801/litex/litex/litex/soc/software/include/hw/common.h:
+/home/hilb/git/unicamp/mo801/litex/litex/litex/soc/software/include/irq.h:
+/home/hilb/git/unicamp/mo801/litex/litex/litex/soc/cores/cpu/vexriscv/irq.h:
+/home/hilb/git/unicamp/mo801/litex/litex/litex/soc/software/libbase/uart.h:
+/home/hilb/git/unicamp/mo801/litex/litex/litex/soc/software/libbase/console.h:
+inference_accel.h:
